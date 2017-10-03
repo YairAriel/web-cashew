df73baf01723e9fe62d9046222035dceab7fcf15
Yair Ariel <Yair Ariel>
2017-10-03 15:57:49 +0300

    first commit

---
+++ src/components/Test.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="test">
+        <h1 class="title is-1">{{ msg }}</h1>
+        <h6>{{ msg }}</h6>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {
+      msg: 'test'
+    }
+  }
+}
+</script>
+
---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/Hello'
+import Calendar from '@/components/Calendar'
+import Test from '@/components/Test'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/calendar',
+      name: 'Calendar',
+      component: Calendar
+    },
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello
+    },
+    {
+      path: '/test',
+      name: 'Test',
+      component: Test
+    }
+
+  ]
+})
